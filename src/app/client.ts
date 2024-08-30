f9bdfddd433814ffe808bc7ecbc5af60bd0f0189
VivekJaiswal18 <96608169+VivekJaiswal18@users.noreply.github.com>
2024-08-31 04:04:52 +0530

    update

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,7 +2,8 @@ import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = process.env.PUBLIC_TEMPLATE_CLIENT_ID;
+const PUBLIC_TEMPLATE_CLIENT_ID='13e3d5fac63e2fa6164efa45ff03b0b9'
+const clientId = PUBLIC_TEMPLATE_CLIENT_ID;
 
 if (!clientId) {
   throw new Error("No client ID provided");
